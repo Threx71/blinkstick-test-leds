af05dd347c236be1974ae8cb2c504313c22b7ce3
Threx71 <gonzalo4795@gmail.com>
2023-12-06 18:11:32 -0300

    updated something (forgot to commit)

--- LED_test.py
+++ LED_test.py
@@ -1,80 +1,98 @@
 from blinkstick import blinkstick
 import time
+import sys
+import traceback
 
-bstk = blinkstick.find_first()
+bstk = None
 num_leds = 16
 
-led_test = False
+def initialize_led():
+    global bstk
+    bstk = blinkstick.find_first()
+    if bstk is None:
+        print("Error: LED strip not found. Please connect the LED strip and restart the program.")
+        sys.exit(1)
 
 def run_led_test():
-    global led_test
+    try:
+        initialize_led()
+        colors = [
+        (255, 255, 255),  # White
+        (255, 0, 0),      # Red
+        (0, 255, 0),      # Green
+        (0, 0, 255),      # Blue
+        (255, 255, 0),    # Yellow
+        (255, 0, 255),    # Magenta
+        (0, 255, 255),    # Cyan
+        (128, 128, 128),  # Gray
+        (128, 0, 0),      # Maroon
+        (0, 128, 0),      # Green (dark)
+        (0, 0, 128),      # Navy
+        (128, 128, 0),    # Olive
+        (128, 0, 128),    # Purple
+        (0, 128, 128),    # Teal
+        (64, 64, 64),     # Dark Gray
+        (255, 165, 0)     # Orange
+    ]
+        
+        for _ in range(3):
+            for color in colors:
+                for i in range(num_leds):
+                    bstk.set_color(channel=0, index=i, red=color[0], green=color[1], blue=color[2])
+                time.sleep(0.3)
 
-    led_test = True
-
-    colors = [
-    (255, 255, 255),  # White
-    (255, 0, 0),      # Red
-    (0, 255, 0),      # Green
-    (0, 0, 255),      # Blue
-    (255, 255, 0),    # Yellow
-    (255, 0, 255),    # Magenta
-    (0, 255, 255),    # Cyan
-    (128, 128, 128),  # Gray
-    (128, 0, 0),      # Maroon
-    (0, 128, 0),      # Green (dark)
-    (0, 0, 128),      # Navy
-    (128, 128, 0),    # Olive
-    (128, 0, 128),    # Purple
-    (0, 128, 128),    # Teal
-    (64, 64, 64),     # Dark Gray
-    (255, 165, 0)     # Orange
-]
-
-
-    while led_test:
-        for color in colors:
-            if not led_test:
-                break
-            for i in range(num_leds):
-                bstk.set_color(channel=0, index=i, red=color[0], green=color[1], blue=color[2])
-            time.sleep(0.3)
-        led_test = False
+    except Exception as e:
+        log_error(e)
+        print("An error occurred. The program will now exit.")
+        sys.exit(1)
 
 def end_light():
-    global led_test
+    try:
+        initialize_led()
+        colors = [
+            (255, 255, 255),
+            (0, 0, 0)
+        ]
+        
+        for _ in range(2):
+            for color in colors:
+                for i in range(num_leds):
+                    bstk.set_color(channel=0, index=i, red=color[0], green=color[1], blue=color[2])
+                time.sleep(0.2)
+    
+    except Exception as e:
+        log_error(e)
+        print("An error occurred. The program will now exit.")
+        sys.exit(1)
 
-    led_test = True
+def log_error(exception):
+    with open("error_log.txt", "a") as f:
+        f.write("Error: {}\n".format(exception))
+        traceback.print_exc(file=f)
 
-    colors = [
-        (255,255,255),
-        (0,0,0)
-
-    ]
-
-    while led_test:
-        for color in colors:
-            if not led_test:
-                break
-            for i in range(num_leds):
-                bstk.set_color(channel=0, index=i, red=color[0], green=color[1], blue=color[2])
-            time.sleep(0.2)
-        led_test = False
-
-print("This test will change the color of the LEDs 2 times for each color Check the LED to see if it works propperly")
-
-user_input = input("\n\nReady to start the LED test, type 'y' to start.\n\n")
-
-if user_input =="y":
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nStarting LED test...")
-    time.sleep(2)
-    for _ in range(3):
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nTest in progress...")
-        run_led_test()
-
-    time.sleep(1)
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nLED test completed")
-    for _ in range(2):
-        end_light()
-    input("\n\nPlease disconnect the LED strip\n\nPress ENTER to close the window")
-else:
-    print("LED test not executed.")
+if __name__ == "__main__":
+    print("This test will change the color of the LEDs 2 times for each color. Check the LED to see if it works properly")
+    
+    user_input = input("\nReady to start the LED test, type 'y' to start.\n")
+    
+    if user_input.lower() == "y":
+        print("\nStarting LED test...")
+        time.sleep(2)
+        try:
+            for _ in range(3):
+                print("\nTest in progress...")
+                run_led_test()
+        
+            time.sleep(1)
+            print("\nLED test completed")
+            for _ in range(2):
+                end_light()
+        
+            input("\nPlease disconnect the LED strip\nPress ENTER to close the window")
+        except KeyboardInterrupt:
+            print("\nLED test interrupted by user.")
+        except Exception as e:
+            log_error(e)
+            print("An error occurred. The program will now exit.")
+    else:
+        print("LED test not executed.")
